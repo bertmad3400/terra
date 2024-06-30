852715d32ba1d86c5579fe379bde98a0c8528661
bertmad3400 <skrivtilbertram@gmail.com>
2024-06-30 16:57:17 +0000

    Add tailwind to project

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,20 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, searchForWorkspaceRoot } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	build: {
+		commonjsOptions: { transformMixedEsModules: true } // Change
+	},
+	server: {
+		fs: {
+			allow: [
+				// search up for workspace root
+				searchForWorkspaceRoot(process.cwd()),
+				// your custom rules
+				'/path/to/custom/allow_directory',
+				'/path/to/custom/allow_file.demo'
+			]
+		}
+	}
 });
