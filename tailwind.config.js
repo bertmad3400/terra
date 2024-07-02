86390ba2d31b91c7bb8b1925f0b74496429d0a50
bertmad3400 <skrivtilbertram@gmail.com>
2024-07-02 13:07:52 +0000

    Add layerchart library

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ import Forms from '@tailwindcss/forms';
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/layerchart/**/*.{svelte,js}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				'surface-100': 'rgb(50 50 50 / <alpha-value>)',
+				'surface-200': 'rgb(100 100 100 / <alpha-value>)',
+				'surface-300': 'rgb(200 200 200 / <alpha-value>)',
+				'surface-content': 'rgb(255 255 255 / <alpha-value>)',
+				danger: 'rgb(255 70 70 / <alpha-value>)'
+			}
+		}
 	},
 	plugins: [Forms]
 };
