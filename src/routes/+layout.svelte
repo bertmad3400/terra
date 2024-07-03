71122bc2d03f970eae80c6d5e170f52eb40fd528
bertmad3400 <skrivtilbertram@gmail.com>
2024-07-03 12:02:10 +0000

    Add OG tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,17 @@
 	import '../app.css';
 </script>
 
+<svelte:head>
+	<title>Terra Mystica Timer</title>
+	<meta property="og:title" content="Terra Mystica Timer" />
+	<meta
+		property="og:description"
+		content="Ever wondered who is the slowest at Terra Mystica? Well, wonder no more..."
+	/>
+	<meta property="og:image" content="https://terra-timer.vercel.app/og-image.png" />
+	<meta property="og:type" content="website" />
+</svelte:head>
+
 <slot />
 
 <style lang="postcss">
